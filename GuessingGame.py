d126a6b726814887a14d0bf93284504c11ca32a3
toor <k.nosek@ievent-management.pl>
2019-07-15 19:32:25 +0000

    create code

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,7 +1,17 @@
 
-awdawdd
-awdawd
-awdwa
-dwadwa
-dwad
-awd
+from random import randint
+answer = None
+
+number = randint(1, 10)
+print(number)
+
+while True:
+    answer = input(("write the nymber between 1 and 9: "))
+    answer = int(answer)
+    if number > answer:
+        print("to low")
+    elif number < answer:
+        print("to high")
+    else:
+        print("Well done, you guys.")
+        break
